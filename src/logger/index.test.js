d184ff503808b1d992272a20e4caddb6234b39fb
Edinho Monteiro <bruno.monteirodg@gmail.com>
2021-04-04 19:34:32 -0300

    test: added tests for logger factory

---
+++ src/logger/index.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+const loggerFactory = require('./index')
+
+describe('logger - index', () => {
+  it('when instaciates the logger returns a instance of Logger', () => {
+    const log = loggerFactory('test')
+    assert.strictEqual('Logger', log.constructor.name)
+  })
+
+  it('throw a Error when try to instanciate without context parameter', () => {
+    assert.throws(() => loggerFactory(), Error)
+  })
+})
