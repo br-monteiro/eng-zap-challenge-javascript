cacd7b1e56410f3676b61b5dcfe736933b57297e
Edinho Monteiro <bruno.monteirodg@gmail.com>
2021-04-04 19:28:44 -0300

    test: added tests for log type 'warning'

---
+++ src/logger/warning-log.test.js
@@ -0,0 +1,37 @@
+const assert = require('assert')
+
+const InfoLog = require('./info-log')
+
+describe('logger - info-log', () => {
+  const bole = {}
+  let logger = {}
+  let outputLog
+
+  beforeEach(() => {
+    bole.info = (message, info) => {
+      outputLog = `${message}#${JSON.stringify(info)}`
+    }
+
+    logger = new InfoLog(bole)
+  })
+
+  it('throws an Error when instanciates without the log engine reference', () => {
+    assert.throws(() => new InfoLog(), Error)
+  })
+
+  it('returns true when the log type "info" was executed', async () => {
+    assert.strictEqual(await logger.log('info', 'test', {}), true)
+  })
+
+  it('returns false when the log type is not "info"', async () => {
+    assert.strictEqual(await logger.log('whatever', 'test', {}), false)
+  })
+
+  it('returns the outputLog equals the parameters values', async () => {
+    const expected = 'this is a log message#{"details":true}'
+
+    await logger.log('info', 'this is a log message', { details: true })
+
+    assert.strictEqual(outputLog, expected)
+  })
+})
