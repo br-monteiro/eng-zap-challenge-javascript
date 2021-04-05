6e4dc48784fff5886ddae2058aa65d5b022ac8d5
Edinho Monteiro <bruno.monteirodg@gmail.com>
2021-04-04 22:47:02 -0300

    test: added tests for buildResponsePattern function

---
+++ src/utils/index.test.js
@@ -0,0 +1,45 @@
+const assert = require('assert')
+const utils = require('./index')
+
+describe('utils - index',  () => {
+  describe('#buildResponsePattern', () => {
+
+    context('returns the object builded according parameters', () => {
+      it('only status', () => {
+        const expected = { status: 'success', message: '' }
+
+        assert.deepStrictEqual(utils.buildResponsePattern('success'), expected)
+      })
+
+      it('with status and message', () => {
+        const expected = { status: 'success', message: 'hello, OLX' }
+
+        assert.deepStrictEqual(utils.buildResponsePattern('success', 'hello, OLX'), expected)
+      })
+
+      it('with status, message and data', () => {
+        const expected = { status: 'success', message: 'hello, OLX', desapega: true }
+
+        assert.deepStrictEqual(utils.buildResponsePattern('success', 'hello, OLX', { desapega: true }), expected)
+      })
+
+      it('with status, message and complex data', () => {
+        const expected = { status: 'error', message: 'hello, OLX', desapega: true, test: 'ok' }
+        const data = {
+          desapega: true,
+          status: 'success',
+          message: 'false',
+          test: 'ok'
+        }
+
+        assert.deepStrictEqual(utils.buildResponsePattern('error', 'hello, OLX', data), expected)
+      })
+    })
+
+    context('throws an Error with invalid status parameter', () => {
+      it('invalid status', () => {
+        assert.throws(() => utils.buildResponsePattern('whatever'), Error)
+      })
+    })
+  })
+})
