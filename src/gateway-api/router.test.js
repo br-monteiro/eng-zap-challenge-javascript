a3e2bd57ed1df59725e1e990be308b358ad32caa
Edinho Monteiro <bruno.monteirodg@gmail.com>
2021-04-10 19:04:04 -0300

    refactor: changed tests assertion

--- src/gateway-api/router.test.js
+++ src/gateway-api/router.test.js
@@ -19,30 +19,53 @@ describe('gateway-api - router', () => {
 
   describe('GET /api/v1/:apikey', () => {
     it('shoud returns HTTP 200 when the APIKey is equals to "viva-real"', (done) => {
+      const expected = {
+        pageNumber: 1,
+        pageSize: 1,
+        totalCount: 1,
+        listings: [{ id: '123' }],
+        status: 'success',
+        message: ''
+      }
+
       chai.request(app)
         .get('/api/v1/viva-real')
         .end((_, res) => {
           res.should.have.status('200')
+          assert.deepStrictEqual(res.body, expected)
           done()
         })
     })
 
     it('shoud returns HTTP 200 when the APIKey is equals to "zap"', (done) => {
+      const expected = {
+        pageNumber: 1,
+        pageSize: 1,
+        totalCount: 1,
+        listings: [{ id: '123' }],
+        status: 'success',
+        message: ''
+      }
+
       chai.request(app)
         .get('/api/v1/zap')
         .end((_, res) => {
           res.should.have.status('200')
+          assert.deepStrictEqual(res.body, expected)
           done()
         })
     })
 
     it('shoud returns HTTP 404 when the APIKey is invalid', (done) => {
+      const expected = {
+        status: 'error',
+        message: 'results not found'
+      }
+
       chai.request(app)
         .get('/api/v1/whatever')
         .end((_, res) => {
           res.should.have.status('404')
-
-          const expected = { status: 'error', message: 'APIKey not found' }
           assert.deepStrictEqual(res.body, expected)
           done()
         })
