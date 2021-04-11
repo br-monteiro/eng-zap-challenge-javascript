fec264250bd8d83d955ed730b0c3b0ceecd7318d
Edinho Monteiro <bruno.monteirodg@gmail.com>
2021-04-11 16:41:26 -0300

    feat: added ZAP processor handler

--- src/input-processor/handlers/set-data-zap.js
+++ src/input-processor/handlers/set-data-zap.js
@@ -1,4 +1,6 @@
 const AbstractHandler = require('./abstract-handler')
+const { setData, getData } = require('../../storage')
+const { isBoundingBoxZap } = require('../../utils')
 
 class SetDataZap extends AbstractHandler {
   /**
@@ -6,7 +8,54 @@ class SetDataZap extends AbstractHandler {
    * @return { AbstractHandler }
    */
   handle (data) {
-    return super.handle(data)
+    try {
+      let id = null
+      let apikey = null
+      let oldData = null
+
+      const businessType = data.pricingInfos.businessType
+      const rentalTotalPrice = Number(data.pricingInfos.rentalTotalPrice)
+
+      const isAvailableForRental = businessType === 'RENTAL' && rentalTotalPrice <= 3500
+      const isAvailableForSale = this.isAvailableForSale(data)
+      const isAvailableToUpdate = this.isAvailableToUpdate(data)
+
+      if (
+        isAvailableToUpdate &&
+        (isAvailableForRental || isAvailableForSale)
+      ) {
+        apikey = 'zap'
+        oldData = getData(apikey, data.id)
+
+        id = setData(apikey, data)
+      }
+
+      return super.handle(data, apikey, id, oldData)
+    } catch (error) {
+      return super.handle(data)
+    }
+  }
+
+  /**
+   * Check if the data is available for rental
+   * @param { InputData } data
+   * @returns { boolean }
+   */
+  isAvailableForSale (data) {
+    const businessType = data.pricingInfos.businessType
+
+    if (businessType !== 'SALE') return false
+
+    const lat = data.address.geoLocation.location.lat
+    const lon = data.address.geoLocation.location.lon
+    const usableAreas = data.usableAreas
+
+    let price = Number(data.pricingInfos.price)
+    price = isBoundingBoxZap(lat, lon) ? price - (price * 0.1) : price
+
+    const squareMeterValue = price / usableAreas
+
+    return squareMeterValue > 0 && squareMeterValue > 3500
   }
 }
 
