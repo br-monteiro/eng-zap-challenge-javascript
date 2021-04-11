ade2de865e82dd81aa4a8088a928ff1a3cc7a8bb
Edinho Monteiro <bruno.monteirodg@gmail.com>
2021-04-11 14:49:57 -0300

    refactor: changed handle assign and added isAvailableToUpdate method

--- src/input-processor/handlers/abstract-handler.js
+++ src/input-processor/handlers/abstract-handler.js
@@ -1,3 +1,5 @@
+const { getData } = require('../../storage')
+const { normalizeStr } = require('../../utils')
 class AbstractHandler {
   constructor () {
     /**
@@ -19,14 +21,33 @@ class AbstractHandler {
 
   /**
    * @param { InputData } data - The data object
+   * @param { string } apikey - The APIKey
+   * @param { string } dataId - The id of data in storage
+   * @param { InputData } oldData - The old register of the data in storage
    * @return { AbstractHandler }
    */
-  handle (data) {
+  handle (data, apikey = null, dataId = null, oldData = null) {
     if (this.nextHandler) {
-      return this.nextHandler.handle(data)
+      return this.nextHandler.handle(data, apikey, dataId, oldData)
     }
 
     return null
+  }
+
+  /**
+   * @param { string } apikey - The APIKey value
+   * @param { InputData } data - The data object
+   */
+  isAvailableToUpdate (apikey, data) {
+    const id = normalizeStr(data && data.id)
+    /**
+     * @type { InputData }
+     */
+    const item = getData(apikey, id)
+
+    if (item === null) return true
+
+    return new Date(item.updatedAt) < new Date(data.updatedAt)
   }
 }
 
