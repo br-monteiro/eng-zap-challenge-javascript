16efa11859fc698ced43e761ce1e58b16c07bc2d
Edinho Monteiro <bruno.monteirodg@gmail.com>
2021-04-11 19:40:18 -0300

    fix: added fallback for server port

--- src/server.js
+++ src/server.js
@@ -3,14 +3,15 @@ require('dotenv').config()
 
 const logger = require('./logger')('server')
 const app = require('./index')
+const port = process.env.PORT || 3000
 
 logger.info('server process starting')
 
-app.listen(process.env.PORT, async error => {
+app.listen(port, async error => {
   if (error) {
     logger.error('unable to listen for connections', error)
     process.exit(10)
   }
 
-  logger.info(`server is listening on port ${process.env.PORT}`)
+  logger.info(`server is listening on port ${port}`)
 })
